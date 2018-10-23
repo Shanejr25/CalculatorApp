b651fae408879a5216c20fea37dd99400ad5cb50
Shanejr <stlazjr0@gmail.com>
2018-10-23 11:11:48 -0700

    Controller, Model, and UI.fxml were updated, and uploaded. Set up basic structure for the controller and model file.

--- Calculator/src/Controller.java
+++ Calculator/src/Controller.java
@@ -1,4 +1,15 @@
 
-public class Controller {
+import javafx.event.ActionEvent;
 
+// main controller class that controls the calculator
+public class Controller {
+	
+	// activates events for the Calculator
+	public void calculateNumbers(ActionEvent event) {
+		
+	}
+	
+	public void processOperators(ActionEvent event) {
+		
+	}
 }
